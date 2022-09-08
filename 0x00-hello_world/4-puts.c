fabcaa14d4cc6ee10819b15b857acc4dac2c5231
Craggy-sense <memusirobi@gmail.com>
2022-09-08 14:00:22 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - to out put the string using put function.
+ *
+ */
+int main(void)
+{
+	
+	put("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
+
