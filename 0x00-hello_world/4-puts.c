de501e7d30c6d0613fb2c287408ec8986d90ac62
Craggy-sense <memusirobi@gmail.com>
2022-09-12 12:36:38 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - to out put the string using put function.
+ * main- Serves as the entry point for the program
  *
- * Description: using main function
- * Programming is like building a multilingual puzzle
- * Return :0
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
  */
 int main(void)
-{
-	
+{	
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 
