6f7506d5536c0621095837c8dec47fedd54e4aff
Craggy-sense <memusirobi@gmail.com>
2022-09-14 14:31:23 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 /**
  * print_sign-prints sign of a number
  * @n: character to be verified
- * Return: return 0 or1
+ * Return: return 0 
  */
 int print_sign(int n)
 {
-if (n == 0)
+if (n > 0)
 {
-_putcher('0');
-return (0);
-}
-else if (n > 0)
-{
-_putchar('+');
+_putcher('+');
 return (1);
 }
-else
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
 }
 }
