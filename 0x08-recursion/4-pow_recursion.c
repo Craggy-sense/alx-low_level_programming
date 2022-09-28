444375ff687c3e1c44d99835521282b1a5048ed7
Craggy-sense <memusirobi@gmail.com>
2022-09-28 13:46:35 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * factorial - returns value of x power y
+ * @n: int value
+ * @y: int value
+ *
+ * Returns: value of x power y
+ */
+
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (pow (x,y));
+}
+
+
+
