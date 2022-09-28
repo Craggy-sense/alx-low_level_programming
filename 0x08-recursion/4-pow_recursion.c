ca869792fad52bb9b5b92f9543c389f69e8afb63
Craggy-sense <memusirobi@gmail.com>
2022-09-28 14:09:26 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,11 @@
 #include <math.h>
 
 /**
- * factorial - returns value of x power y
- * @n: int value
- * @y: int value
- *
- * Returns: value of x power y
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
 
 
