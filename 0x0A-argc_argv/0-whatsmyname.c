6e3a22af7262aa7a8d1c3e13c24ac0fc1dc75332
Craggy-sense <memusirobi@gmail.com>
2022-10-01 12:13:22 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 /**
  * main - program prints its name, followed by a new line
- * Description: if name is renamed the program will print new name without compling
- * Return: new name
+ * Description: if name is renamed the program will print new name
+ * @argc: argurment count
+ * @argv: argument vector
+ * Return: int
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
