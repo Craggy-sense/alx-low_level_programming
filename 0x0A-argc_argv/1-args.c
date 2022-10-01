e1c57dfdceed9fd46160a0dae8bb141f37115850
Craggy-sense <memusirobi@gmail.com>
2022-10-01 12:20:37 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main: prints the number of arguments passed into it
+ * description: program should print a number, followed by a new line
+ * @argc: number
+ * @argv: vector
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d", argc);
+	return (0);
+}
